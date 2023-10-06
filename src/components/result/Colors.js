eb6e802f291bfa140fb85f36962a35e4a1eb22fd
hidexxxxx <hidetaka54522@gmail.com>
2023-10-06 10:19:04 +0900

    ColorsUpdated

--- src/components/result/Colors.js
+++ src/components/result/Colors.js
@@ -33,11 +33,17 @@ class Colors extends Component {
   renderBrownDef() {
     return (
       <BrownDef
-        title={'Brown Type Definition'}
-        content={`You are a Builder. You enjoy leading, creating and working hard.
-                  You are a traditional person with respect for authority.
-                  Your strengths are your diligence, directness and practicality.
-                  Your weaknesses are your lack of tact, patience, and difficulty with abstractions.`}
+        // title={'Brown Type Definition'}
+        // content={`You are a Builder. You enjoy leading, creating and working hard.
+        //           You are a traditional person with respect for authority.
+        //           Your strengths are your diligence, directness and practicality.
+        //           Your weaknesses are your lack of tact, patience, and difficulty with abstractions.`}
+        title={'ブラウンタイプの特徴'}
+        content={`あなたは指導者タイプです。リーダーシップを楽しみ、創造的に働きます。
+          伝統的な価値観を持ち、権威を尊重します。
+          あなたの強みは勤勉さ、率直さ、実践的な考え方です。
+          あなたの弱点は遠慮のなさ、忍耐力の不足、抽象的な概念の理解に苦労することが時々ある点です。`}
+
         onBackClick={this.onBrownDefClick}
       />
     )
@@ -46,11 +52,17 @@ class Colors extends Component {
   renderGreenDef() {
     return (
       <GreenDef
-        title={'Green Type Definition'}
-        content={`You are a Planner. You enjoy dreaming, plotting and innovating.
-                  You tend to spend a lot of time thinking.
-                  Your strengths are your vision, objectiveness and attention to detail.
-                  Your weaknesses are your difficulty in putting yourself in the current moment and lack of practicality and speed.`}
+        // title={'Green Type Definition'}
+        // content={`You are a Planner. You enjoy dreaming, plotting and innovating.
+        //           You tend to spend a lot of time thinking.
+        //           Your strengths are your vision, objectiveness and attention to detail.
+        //           Your weaknesses are your difficulty in putting yourself in the current moment and lack of practicality and speed.`}
+        title={'グリーンタイプの特徴'}
+        content={`あなたはプランナータイプです。夢を見たり、計画を立てたり、革新的なことを楽しむことが多いです。
+          多くの時間を考えることに費やす傾向があります。
+          あなたの強みはビジョン、客観性、細部への注意深さです。
+          あなたの弱点は今に集中することの難しさ、実用性とスピードが少し不足していることが時々ある点です。`}
+
         onBackClick={this.onGreenDefClick}
       />
     )
@@ -59,11 +71,17 @@ class Colors extends Component {
   renderBlueDef() {
     return (
       <BlueDef
-        title={'Blue Type Definition'}
-        content={`You are a Relater. You enjoy chatting, romance and spending time with others.
-                You are an empathetic person who tends to put the needs of others above your own.
-                Your strengths are your sympathy, openness and awareness of your own emotions.
-                Your weaknesses are your subjectivity, pliability and manipulability.`}
+        // title={'Blue Type Definition'}
+        // content={`You are a Relater. You enjoy chatting, romance and spending time with others.
+        //         You are an empathetic person who tends to put the needs of others above your own.
+        //         Your strengths are your sympathy, openness and awareness of your own emotions.
+        //         Your weaknesses are your subjectivity, pliability and manipulability.`}
+        title={'ブルータイプの特徴'}
+        content={`あなたはコミュニケーションタイプです。おしゃべりやロマンス、他人との時間を楽しむことが多いです。
+          他人のニーズを自分よりも優先する共感力のある人です。
+          あなたの強みは同情心、オープンさ、自分の感情に対する意識です。
+          あなたの弱点は主観性や柔軟性、他に人に操作されやすい点が時々ある点です。`}
+
         onBackClick={this.onBlueDefClick}
       />
     )
@@ -72,11 +90,17 @@ class Colors extends Component {
   renderRedDef() {
     return (
       <RedDef
-        title={'Red Type Definition'}
-        content={`You are an Adventurer. You enjoy action, excitement and drama.
-                  You easily accept change and are spontaneous.
-                  Your strengths are your tenacity, fearlessness and adaptability.
-                  Your weaknesses are your carelessness and lack of focus and diligence.`}
+        // title={'Red Type Definition'}
+        // content={`You are an Adventurer. You enjoy action, excitement and drama.
+        //           You easily accept change and are spontaneous.
+        //           Your strengths are your tenacity, fearlessness and adaptability.
+        //           Your weaknesses are your carelessness and lack of focus and diligence.`}
+        title={'レッドタイプの特徴'}
+        content={`あなたは冒険的なタイプです。アクションや興奮、ドラマを楽しむことが多いです。
+          変化を受け入れやすく、思いつきで行動することがあります。
+          あなたの強みは粘り強さ、恐れ知らずさ、適応力です。
+          あなたの弱点は不注意さ、焦点の欠如、そして注意深さが不足していることが時々ある点です。`}
+
         onBackClick={this.onRedDefClick}
       />
     )
