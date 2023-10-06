c0963f626b0bd5e47043512d8491da13a95c760e
hidexxxxx <hidetaka54522@gmail.com>
2023-10-06 10:33:27 +0900

    LettersUploaded

--- src/components/result/Letters.js
+++ src/components/result/Letters.js
@@ -33,10 +33,14 @@ class Letters extends Component {
   renderLetterA() {
     return (
       <LetterA
-        title={'Letter A Definition'}
-        content={`You are a type A personality. You like to be in control of everything.
-                Your strengths are your tenacity, confidence and practicality.
-                Your weaknesses are your lack of empathy, discipline and detail-awareness.`}
+        // title={'Letter A Definition'}
+        // content={`You are a type A personality. You like to be in control of everything.
+        //         Your strengths are your tenacity, confidence and practicality.
+        //         Your weaknesses are your lack of empathy, discipline and detail-awareness.`}
+        title={'Aタイプの特徴'}
+        content={`あなたはAタイプの性格です。何事もコントロールしたがる傾向があります。
+          あなたの強みは粘り強さ、自信、実践的な考え方です。
+          あなたの弱点は共感力の不足、自己規律の欠如、細部への注意不足などが時々ありそうです。`}
         onBackClick={this.onLetterA_Click}
       />
     )
@@ -45,10 +49,15 @@ class Letters extends Component {
   renderLetterB() {
     return (
       <LetterB
-        title={'Letter B Definition'}
-        content={`You are a type B personality. You like to be around people.
-                Your strengths are your empathy, enthusiasm and spontaneity.
-                Your weaknesses are your lack of patience, attention and confidence.`}
+        // title={'Letter B Definition'}
+        // content={`You are a type B personality. You like to be around people.
+        //         Your strengths are your empathy, enthusiasm and spontaneity.
+        //         Your weaknesses are your lack of patience, attention and confidence.`}
+        title={'Bタイプの特徴'}
+        content={`あなたはBタイプの性格です。人と一緒にいることを好みます。
+          あなたの強みは共感力、熱意、自発性です。
+          あなたの弱点は忍耐力の不足、注意力の不足、自信の不足などが時々ありそうです。`}
+
         onBackClick={this.onLetterB_Click}
       />
     )
@@ -57,10 +66,15 @@ class Letters extends Component {
   renderLetterC() {
     return (
       <LetterC
-        title={'Letter C Definition'}
-        content={`You are a type C personality. You like to think things through.
-                Your strengths are your rationality, objectivity and originality.
-                Your weaknesses are your lack of spontaneity, practicality and empathy.`}
+        // title={'Letter C Definition'}
+        // content={`You are a type C personality. You like to think things through.
+        //         Your strengths are your rationality, objectivity and originality.
+        //         Your weaknesses are your lack of spontaneity, practicality and empathy.`}
+        title={'Cタイプの特徴'}
+        content={`あなたはCタイプの性格です。物事を熟考することを好みます。
+          あなたの強みは論理的思考、客観性、独創性です。
+          あなたの弱点は自発性の不足、実践性の不足、共感力の不足などが時々ありそうです。`}
+
         onBackClick={this.onLetterC_Click}
       />
     )
@@ -69,10 +83,14 @@ class Letters extends Component {
   renderLetterD() {
     return (
       <LetterD
-        title={'Letter D Definition'}
-        content={`You are a type D personality. You like to feel safe and secure in life.
-                  Your strengths are your organization, compassion and patience.
-                  Your weaknesses are your lack of ambition, adaptability and assertiveness.`}
+        // title={'Letter D Definition'}
+        // content={`You are a type D personality. You like to feel safe and secure in life.
+        //           Your strengths are your organization, compassion and patience.
+        //           Your weaknesses are your lack of ambition, adaptability and assertiveness.`}
+        title={'Dタイプの特徴'}
+        content={`あなたはDタイプの性格です。日々の生活の中で安全と安定感を求める傾向があります。
+          あなたの強みは組織力、思いやり、忍耐力です。
+          あなたの弱点は野心の不足、適応力の不足、自己主張の不足などが時々ありそうです。`}
         onBackClick={this.onLetterD_Click}
       />
     )
